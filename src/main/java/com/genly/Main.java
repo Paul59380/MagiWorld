b8f8c9ecd97c579ad414c4cf9fc81c0677104de4
Paul59380 <paul.genly59380@gmail.com>
2019-05-28 09:44:13 +0200

    Add two attributes in Main for create the player one and player two. Create the method addNewsPlayer for add a new object if is null

--- src/main/java/com/genly/Main.java
+++ src/main/java/com/genly/Main.java
@@ -3,37 +3,60 @@ package com.genly;
 import java.util.ArrayList;
 
 public class Main {
+    private Characters playerOne;
+    private Characters playerTwo;
+
+    public void setPlayerOne(Characters playerOne) {
+        this.playerOne = playerOne;
+    }
+
+    public void setPlayerTwo(Characters playerTwo) {
+        this.playerTwo = playerTwo;
+    }
 
     public static void main(String[] args) {
+        Main main = new Main();
         for (int i = 0; i != 2; i++) {
             System.out.println("Création du personnage " + (i + 1) + " :");
             Order order = new Order();
             ArrayList<String> characters = new ArrayList<String>(order.runChoiceCharacter());
             if ("Guerrier".equals(characters.get(5))) {
-                Warrior warrior = new Warrior(Integer.parseInt(characters.get(0)),
+                Characters warrior = new Warrior(Integer.parseInt(characters.get(0)),
                         Integer.parseInt(characters.get(1)),
                         Integer.parseInt(characters.get(2)),
                         Integer.parseInt(characters.get(3)),
                         Integer.parseInt(characters.get(4)),
                         characters.get(5));
                 warrior.description();
+                main.addNewPlayer(warrior);
             } else if ("Rôdeur".equals(characters.get(5))) {
-                Walker walker = new Walker(Integer.parseInt(characters.get(0)),
+                Characters walker = new Walker(Integer.parseInt(characters.get(0)),
                         Integer.parseInt(characters.get(1)),
                         Integer.parseInt(characters.get(2)),
                         Integer.parseInt(characters.get(3)),
                         Integer.parseInt(characters.get(4)),
                         characters.get(5));
                 walker.description();
+                main.addNewPlayer(walker);
             } else {
-                Magus magus = new Magus(Integer.parseInt(characters.get(0)),
+                Characters magus = new Magus(Integer.parseInt(characters.get(0)),
                         Integer.parseInt(characters.get(1)),
                         Integer.parseInt(characters.get(2)),
                         Integer.parseInt(characters.get(3)),
                         Integer.parseInt(characters.get(4)),
                         characters.get(5));
                 magus.description();
+                main.addNewPlayer(magus);
             }
         }
+        System.out.println(main.playerOne.getType());
+        System.out.println(main.playerTwo.getType());
+    }
+
+    public void addNewPlayer(Characters characters) {
+        if(playerOne == null)
+            setPlayerOne(characters);
+        else
+            setPlayerTwo(characters);
     }
 }
--- src/main/java/com/genly/Order.java
+++ src/main/java/com/genly/Order.java
@@ -1,8 +1,6 @@
 package com.genly;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 public class Order {
